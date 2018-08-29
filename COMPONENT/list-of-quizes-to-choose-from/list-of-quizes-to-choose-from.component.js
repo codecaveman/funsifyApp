e876045e87e1c5795fc54d0909fb512cf27efd3d
EFlynn <efpdfexpert @gmail.com>
2018-08-29 13:20:24 +0100

    update list of quizzes from firestore

--- COMPONENT/list-of-quizes-to-choose-from/list-of-quizes-to-choose-from.component.js
+++ COMPONENT/list-of-quizes-to-choose-from/list-of-quizes-to-choose-from.component.js
@@ -1,7 +1,31 @@
 angular.module("listOfQuizesToChooseFrom", []).
 component('listOfQuizesToChooseFrom', {
     templateUrl: 'COMPONENT/list-of-quizes-to-choose-from/list-of-quizes-to-choose-from.template.html',
-    controller: function() {
+    controller: function($scope) {
+      
+  	  $scope.listTitles = [4,2,1]
+     const listTitlesRef = funsifyDatabase.collection("games").doc("game1");
+    
+      listTitlesRef.get().then(function(doc) { 
+      	if (doc.exists) { 
+      	
+      	
+      	
+      	  alert(doc.data().list[2])
+      	   $scope.$apply(function () {
+            $scope.listTitles = doc.data().list;
+      });
+      	  
+      	
+      		} else { 
+      		// doc.data() will be undefined in this case 
+      		alert("No such document!"); 
+      		} 
+      	}).catch(function(error) { 
+      		alert("Error getting document:"); });
+    
+ 
+    
     	this.w3css = {
     	header:"w3-top w3-black",
     	activeList:"",
