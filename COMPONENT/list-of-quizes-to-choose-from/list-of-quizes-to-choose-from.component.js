bb6647ec64bc92ee2e82ddf16ca0be25b3b8646b
EFlynn <efpdfexpert @gmail.com>
2018-08-29 14:17:21 +0100

    get all list docs from firestore and display

--- COMPONENT/list-of-quizes-to-choose-from/list-of-quizes-to-choose-from.component.js
+++ COMPONENT/list-of-quizes-to-choose-from/list-of-quizes-to-choose-from.component.js
@@ -1,39 +1,24 @@
 angular.module("listOfQuizesToChooseFrom", []).
 component('listOfQuizesToChooseFrom', {
-    templateUrl: 'COMPONENT/list-of-quizes-to-choose-from/list-of-quizes-to-choose-from.template.html',
+		templateUrl: 'COMPONENT/list-of-quizes-to-choose-from/list-of-quizes-to-choose-from.template.html',
     controller: function($scope) {
-      
-  	  $scope.listTitles = [4,2,1]
-     const listTitlesRef = funsifyDatabase.collection("games").doc("game1");
-    
-      listTitlesRef.get().then(function(doc) { 
-      	if (doc.exists) { 
-      	
-      	
-      	
-      	  alert(doc.data().list[2])
-      	   $scope.$apply(function () {
-            $scope.listTitles = doc.data().list;
-      });
-      	  
-      	
-      		} else { 
-      		// doc.data() will be undefined in this case 
-      		alert("No such document!"); 
-      		} 
-      	}).catch(function(error) { 
-      		alert("Error getting document:"); });
-    
- 
-    
-    	this.w3css = {
-    	header:"w3-top w3-black",
-    	activeList:"",
-    	list:"",
-    	button: "w3-btn w3-xxlarge w3-center w3-blue",
-			footer:"w3-bottom w3-black",
+			$scope.listTitles = []
+			const listTitlesRef = funsifyDatabase.collection("lists");
+			listTitlesRef.get().then(function(querySnapshot) { 
+				querySnapshot.forEach(function(doc) {
+					$scope.$apply(function () {
+					$scope.listTitles.push(doc.id) 
+					});
+				}); 
+			});
+			this.w3css = {
+				header:"w3-top w3-black",
+				activeList:"",
+				list:"",
+				button: "w3-btn w3-xxlarge w3-center w3-blue",
+				footer:"w3-bottom w3-black",
 			} // end this.w3css
-    } // end controller function
+		} // end controller function
 }); // end component
 
 
